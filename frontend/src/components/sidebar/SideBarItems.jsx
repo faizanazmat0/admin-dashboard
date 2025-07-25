b0f7605b3b29820b6f750850b7493183e902cc84
Faizan Azmat <faizanazmat0@gmail.com>
2025-07-25 19:52:58 +0500

    Build next portion of the sidebar

--- frontend/src/components/sidebar/SideBarItems.jsx
+++ frontend/src/components/sidebar/SideBarItems.jsx
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
-import { LayoutDashboard, ChevronUp, ChevronDown } from "lucide-react";
+import {
+  LayoutDashboard,
+  ChevronUp,
+  ChevronDown,
+  CalendarDays,
+  MessagesSquare,
+  MailOpen,
+  ShoppingCart,
+  GraduationCap,
+  Share2,
+  Trello,
+  TicketPlus,
+} from "lucide-react";
 import { Link } from "react-router";
 import "./sidebaritems.css";
 
@@ -27,22 +39,92 @@ const SideBarItems = () => {
       {/* Dropdown Content Section */}
       {isDashboardOpen && (
         <div className="dropdownContent">
-          <div className="dropdownItem active">Default</div>
-          <div className="dropdownItem">Analytics</div>
-          <div className="dropdownItem">CRM</div>
-          <div className="dropdownItem">E commerce</div>
-          <div className="dropdownItem withTag">
+          <Link className="dropdownItem active">Default</Link>
+          <Link className="dropdownItem">Analytics</Link>
+          <Link className="dropdownItem">CRM</Link>
+          <Link className="dropdownItem">E commerce</Link>
+          <Link className="dropdownItem withTag">
             LMS
             <span className="newTag">NEW</span>
-          </div>
-          <div className="dropdownItem">Management</div>
-          <div className="dropdownItem">Saas</div>
-       <div className="dropdownItem withTag">
-           Support desk
+          </Link>
+          <Link className="dropdownItem">Management</Link>
+          <Link className="dropdownItem">Saas</Link>
+          <Link className="dropdownItem withTag">
+            Support desk
             <span className="newTag">NEW</span>
-          </div>
+          </Link>
         </div>
       )}
+
+      {/* App Section  */}
+
+      <div className="appContainer">
+        <div className="appHeading">
+          <h5>App</h5>
+          <div className="headingUnderLine"></div>
+        </div>
+        <div className="appListings">
+          <div>
+            <Link className="appLink">
+              <CalendarDays size={18} />
+              Calendar
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <MessagesSquare size={18} />
+              Chat
+            </Link>
+          </div>
+          <div>
+            <Link className="appLink">
+              <MailOpen size={18} />
+              Email
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <CalendarDays size={18} />
+              Events
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <ShoppingCart size={18} />E commerce
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <GraduationCap size={18} />E learning
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <Trello size={18} />
+              Kanban
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <Share2 size={18} />
+              Social
+            </Link>
+          </div>
+
+          <div>
+            <Link className="appLink">
+              <TicketPlus size={18} />
+              Support desk
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
